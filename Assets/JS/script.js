71170de99328f6aef6df621a0863ffa4abbb7baf
dmod129 <derek.modugno@gmail.com>
2023-08-31 15:59:37 -0500

    added endGame function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,18 +1,31 @@
 var carousel = document.querySelector("#quizBox");
 var next = document.querySelector(".next");
 var prev = document.querySelector(".prev");
+var scoreBox = document.querySelector(".scoreBox");
+var initials = document.querySelector(".initials");
+
 
 // Start Button
 var startBtn = document.querySelector('.startBtn');
 startBtn.addEventListener('click', startGame)
 
+// Submit Button
+var submit = document.querySelector("#submit");
+submit.addEventListener("click", function() {
+    console.log("info saved");
+    // Set local Storage
+} );
+
 // Q & A
 var questionElement = document.querySelector("#question");
 var responseElement = document.querySelector("#responses");
 var index = 0;
+var score = 0;
+var counter = 90;
 
 var timerElement = document.querySelector(".timerSec");
 
+// Start Game Function
 function startGame() {
    console.log('game started');
    startBtn.classList.add('hide');
@@ -23,8 +36,8 @@ function startGame() {
    startTimer();
 }
 
+// Start Timer
 function startTimer() {
-   var counter = 90;
    var timerInterval = setInterval(function() {
        counter--;
        if (counter >= 0) {
@@ -51,9 +64,6 @@ function renderResponses() {
    console.log(questions[0].responses[i]);
    var li = document.createElement("li");
    li.textContent = questions[0].responses[i];
-   li.addEventListener("click", function(event) {
-    handleResponseClick(event.target.textContent)
-   });
    responseElement.appendChild(li);
    }
 }
@@ -63,39 +73,51 @@ function handleResponseClick(clickedResponse) {
     var currentQuestion = questions[index];
     if (clickedResponse === currentQuestion.responses[currentQuestion.answer]) {
         console.log("Correct answer!");
+        score += 5;
     } else {
         console.log("Incorrect answer.");
+        counter -= 10;
+        // Subtract 10 seconds from timer
     }
  
     // Navigate to the next question
     navigate(1);
-    renderQuestionAndResponses();
  };
  
+//  Response event listener
+responseElement.addEventListener("click", function(event) {
+    console.log(event.target.tagName);
+    if (event.target.tagName = "LI") {
+      handleResponseClick(event.target.textContent);  
+    }
+})
 
 // Generate questions
 var questions = [
    { question: "What is a function?", 
      responses: [ "Resuable code", "Primitive value", "None of the above" ], answer: 0 },
+
     { question: "What is an array?", 
     responses: [ "List of values", "Key value pairs", "None of the above" ], answer: 2 },
+
     { question: "What is a primitive value?", 
     responses: [ "123", "1234", "None of the above" ], answer: 2 },
+
     { question: "What is the abbreviation JSON?", 
     responses: [ "JASON", "Javascript notation object", "None of the above" ], answer: 1 },
   ];
 
 // Carousel
 
-next.addEventListener("click", function() {
-   navigate(1);
-   renderQuestionAndResponses();
-});
+// next.addEventListener("click", function() {
+//    navigate(1);
+//    renderQuestionAndResponses();
+// });
 
-prev.addEventListener("click", function() {
-   navigate(-1);
-   renderQuestionAndResponses();
-});
+// prev.addEventListener("click", function() {
+//    navigate(-1);
+//    renderQuestionAndResponses();
+// });
 
 // Render Questions and Responses
 function renderQuestionAndResponses() {
@@ -110,7 +132,7 @@ function renderQuestionAndResponses() {
    }
 }
   
-//    // Navigate through list of questions
+// Navigate through list of questions
 
 function navigate(direction) {
    index = index + direction;
@@ -122,8 +144,18 @@ function navigate(direction) {
       // If you are at the very end. 
       // Go to the first image/question
       else if (index > questions.length - 1) { 
-      index = 0;
+      endGame();
+    } else {
+        renderQuestionAndResponses();
     }
     
    }
 
+// End Game Function
+function endGame() {
+    // Give Score 
+    quizBox.style.display = "none";
+    scoreBox.textContent = score;
+
+    // Ask for initials
+}
